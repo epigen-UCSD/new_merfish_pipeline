1afcd151d0a069b3fae799207799e85fa8fc591d
Colin Kern <colin.kern@gmail.com>
2024-02-16 15:04:36 -0800

    Improved client/server scripts

--- client.py
+++ client.py
@@ -4,7 +4,6 @@ import os
 import multiprocessing
 from ioMicro import read_im, get_local_max_tile, get_dapi_features
 import numpy as np
-import cv2
 import json
 import logging
 
@@ -53,39 +52,22 @@ def worker(name, gpu):
     while True:
         try:
             img_nas, img_file, save_nas, save_fl, psf_file, med_file, icol = server.request(name, gpu)
-            img_file = os.path.join(config["nas-mapping"][img_nas], img_file)
             save_fl = os.path.join(config["nas-mapping"][save_nas], save_fl)
             if os.path.exists(save_fl):
                 continue
-            if not os.path.exists(os.path.dirname(save_fl)):
-                os.makedirs(os.path.dirname(save_fl), exist_ok=True)
-                os.chmod(os.path.dirname(save_fl), 0o777)
             logging.info(f"Creating {save_fl} from {img_file}")
+            img_file = os.path.join(config["nas-mapping"][img_nas], img_file)
+            psf_file = os.path.join(config["nas-mapping"][save_nas], psf_file)
+            med_file = os.path.join(config["nas-mapping"][save_nas], med_file)
             if psf_file not in psfs:
-                if os.path.exists(psf_file):
-                    psfs[psf_file] = np.load(psf_file)
-                else:
-                    logging.info(f"Getting PSF {psf_file}")
-                    psfs[psf_file] = np.array(server.get_psf(psf_file))
-                    psfs[psf_file].save(psf_file)
-            if (med_file, icol) not in meds:
-                if os.path.exists(f"{med_file}{icol}.npz"):
-                    meds[med_file, icol] = np.array(np.load(f"{med_file}{icol}.npz")["im"], dtype=np.float32)
-                    meds[med_file, icol] = cv2.blur(meds[med_file, icol], (20, 20))
-                else:
-                    logging.info(f"Getting median image {med_file} for color {icol}")
-                    meds[med_file, icol] = np.array(server.get_flat_field(med_file, icol))
+                psfs[psf_file] = np.load(psf_file)
+            if med_file not in meds:
+                meds[med_file] = np.load(med_file)
             if "Xhfits" in save_fl:
-                compute_fits(img_file, icol, save_fl, psfs[psf_file], meds[med_file, icol], gpu, name)
+                compute_fits(img_file, icol, save_fl, psfs[psf_file], meds[med_file], gpu, name)
             elif "dapiFeatures" in save_fl:
                 get_dapi_features(
-                    img_file,
-                    os.path.dirname(save_fl),
-                    "",
-                    gpu=False,
-                    im_med_fl=meds[med_file, icol],
-                    psf_fl=psfs[psf_file],
-                    name=name
+                    img_file, os.path.dirname(save_fl), "", gpu=False, im_med_fl=med_file, psf_fl=psf_file, name=name
                 )
             server.complete(name)
         except xmlrpc.client.Fault:
--- server.py
+++ server.py
@@ -8,29 +8,12 @@ import numpy as np
 import multiprocessing
 import cv2
 import curses
+import json
+import sys
 
-data_nas = "merfish12"
-data_folders = r"20231024_RD129_N5S2heart/RMERFISH/H*"
-skip = []
-save_nas = "merfish12"
-save_folder = "20231024_RD129_N5S2heart_MERFISH_analysis"
-psf_file = r"psfs/psf_D103_B.npy"
-flat_field_fl = r"flat_field/RD129_H2_RMER__med_col_raw"
-# If order="hyb", fitting will be done for all FOVs in H1, then all FOVs in H2, etc.
-# If order="fov", fitting will be done for all hybs of FOV 0, then all hybs of FOV 1, etc.
-order = "hyb"
-ncol = 4
-
-hostname = "breadfruit.ucsd.edu"
-port = 8000
-
-naspath = {
-    "merfish9": "/mnt/merfish9",
-    "merfish10": "/mnt/merfish10",
-    "merfish11": "/mnt/merfish11",
-    "merfish12": "/mnt/merfish12",
-    "merfish13": "/mnt/merfish13",
-}
+config = json.load(open(sys.argv[1]))
+data_nas = config["nas-mapping"][config["data_nas"]]
+save_nas = config["nas-mapping"][config["save_nas"]]
 
 
 def atoi(text):
@@ -50,33 +33,50 @@ class TaskServer:
         self.messages = messages
 
     def scan_folders(self):
-        all_flds = glob.glob(os.path.join(naspath[data_nas], data_folders) + os.sep)
-        all_flds.sort(key=natural_keys)
+        all_folders = glob.glob(os.path.join(data_nas, config["data_folders"]) + os.sep)
+        all_folders.sort(key=natural_keys)
         self.tasks = []
-        self.dapi_features = []
         total = 0
-        for fld in all_flds:
-            self.messages.put(["Scanning", fld])
-            if fld in skip:
+        for folder in all_folders:
+            self.messages.put(["Scanning", folder])
+            if folder in config["skip_hybs"]:
                 continue
-            for filename in sorted(glob.glob(fld + os.sep + "*.zarr")):
-                filename_no_nas = filename.split(naspath[data_nas])[-1].strip(os.sep)
-                fov = os.path.basename(filename).split(".")[0]
-                hyb = filename.split("/")[-2]
-                prefix = os.path.join(save_folder, f"{fov}--{hyb}")
-                for icol in range(ncol - 1):
-                    save_fl = f"{prefix}--col{icol}__Xhfits.npz"
-                    total += 1
-                    if not os.path.exists(os.path.join(naspath[save_nas], save_fl)):
-                        self.tasks.append((data_nas, filename_no_nas, save_nas, save_fl, psf_file, flat_field_fl, icol))
-                save_fl = f"{prefix}--dapiFeatures.npz"
-                total += 1
-                if not os.path.exists(os.path.join(naspath[save_nas], save_fl)):
-                    self.tasks.append((data_nas, filename_no_nas, save_nas, save_fl, psf_file, flat_field_fl, ncol - 1))
+            for filename in sorted(glob.glob(folder + os.sep + "*.zarr")):
+                total += self.scan_file(filename)
                 self.messages.put(["Tasks", len(self.tasks), total])
-        if order == "fov":
+        if config["order"] == "fov":
             self.tasks.sort(key=parse_fov)
         self.messages.put(["Scanning", ""])
+
+    def create_task(self, image_file, save_fl, icol):
+        self.tasks.append(
+            (
+                config["data_nas"],
+                image_file,
+                config["save_nas"],
+                save_fl,
+                os.path.join(config["save_folder"], os.path.basename(config["psf_file"])),
+                os.path.join(config["save_folder"], os.path.basename(config["flat_field_fl"])) + f"{icol}.npy",
+                icol,
+            )
+        )
+
+    def scan_file(self, filename):
+        total = 0
+        image_file = filename.split(data_nas)[-1].strip(os.sep)
+        fov = os.path.basename(filename).split(".")[0]
+        hyb = filename.split("/")[-2]
+        prefix = os.path.join(config["save_folder"], f"{fov}--{hyb}")
+        for icol in range(config["ncol"] - 1):
+            save_fl = f"{prefix}--col{icol}__Xhfits.npz"
+            total += 1
+            if not os.path.exists(os.path.join(save_nas, save_fl)):
+                self.create_task(image_file, save_fl, icol)
+        save_fl = f"{prefix}--dapiFeatures.npz"
+        total += 1
+        if not os.path.exists(os.path.join(save_nas, save_fl)):
+            self.create_task(image_file, save_fl, config["ncol"] - 1)
+        return total
 
     def request(self, client, nodapi=False):
         i = 0
@@ -93,20 +93,21 @@ class TaskServer:
     def complete(self, client):
         self.messages.put(["Completed", client])
 
-    def get_psf(self, psf_file):
-        return np.load(psf_file).tolist()
-
-    def get_flat_field(self, flat_field_fl, icol):
-        fl_med = f"{flat_field_fl}{icol}.npz"
-        im_med = np.array(np.load(fl_med)["im"], dtype=np.float32)
-        return cv2.blur(im_med, (20, 20)).tolist()
-
     def start(self):
         self.scan_folders()
-        with SimpleXMLRPCServer((hostname, port), logRequests=False) as server:
+        save_folder = os.path.join(save_nas, config["save_folder"])
+        if not os.path.exists(save_folder):
+            os.makedirs(save_folder, exist_ok=True)
+            os.chmod(save_folder, 0o777)
+        psf = np.load(config["psf_file"])
+        np.save(os.path.join(save_folder, os.path.basename(config["psf_file"])), psf)
+        for icol in range(config["ncol"]):
+            fl_med = f"{config['flat_field_fl']}{icol}.npz"
+            im_med = np.array(np.load(fl_med)["im"], dtype=np.float32)
+            im_med = cv2.blur(im_med, (20, 20))
+            np.save(os.path.join(save_folder, os.path.basename(config["flat_field_fl"]) + f"{icol}.npy"), im_med)
+        with SimpleXMLRPCServer((config["server-hostname"], config["server-port"]), logRequests=False) as server:
             server.register_function(self.request)
-            server.register_function(self.get_psf)
-            server.register_function(self.get_flat_field)
             server.register_function(self.complete)
             server.serve_forever()
 
@@ -116,67 +117,84 @@ def server_process(messages):
     server.start()
 
 
-def interface(stdscr, messages):
+class AnalysisStatus:
+    def __init__(self):
+        self.scanning = ""
+        self.worker_status = {}
+        self.worker_started = {}
+        self.durations = []
+        self.done = None
+        self.tasks = 0
+        self.total = 1
+
+    def parse_message(self, message):
+        if message[0] == "Scanning":
+            self.scanning = message[1]
+        elif message[0] == "Assigning":
+            self.worker_status[message[2]] = message[1][3]
+            self.worker_started[message[2]] = time.time()
+        elif message[0] == "Completed":
+            if message[1] in self.worker_started:
+                self.durations.append(time.time() - self.worker_started[message[1]])
+                self.update_time_remaining()
+                del self.worker_started[message[1]]
+            if message[1] in self.worker_status:
+                del self.worker_status[message[1]]
+        elif message[0] == "Tasks":
+            self.tasks = message[1]
+            self.total = message[2]
+
+    def update_time_remaining(self):
+        avg = sum(self.durations[-100:]) / len(self.durations[-100:])
+        secs_left = (avg * self.tasks) / len(self.worker_status)
+        self.td = datetime.timedelta(seconds=int(secs_left))
+        self.done = datetime.datetime.now() + self.td
+        self.tasks -= 1
+
+    def progress(self):
+        if self.scanning:
+            return f"{self.tasks} files left to generate, scanning {self.scanning}"
+        pct = 100 * (self.total - self.tasks) / self.total
+        return f"{self.tasks} files left to generate, {pct:0.1f}% complete"
+
+    def completion(self):
+        if self.done:
+            return f"Estimated completion in {self.td} at {self.done}"
+        return "Estimated completion: TBD"
+
+    def workers(self):
+        if not self.worker_status:
+            yield "Waiting for task requests"
+        for name, status in self.worker_status.items():
+            duration = datetime.timedelta(seconds=int(time.time() - self.worker_started[name]))
+            yield f"{name}: {status} ({duration})"
+
+
+def main(stdscr, messages):
     stdscr.clear()
     curses.curs_set(0)
     stdscr.nodelay(True)
-
-    scanning = ""
-    worker_status = {}
-    worker_started = {}
-    durations = []
-    done = None
-    tasks = 0
-    total = 1
+    status = AnalysisStatus()
     clear_counter = 0
     while True:
         while not messages.empty():
-            message = messages.get()
-            if message[0] == "Scanning":
-                scanning = message[1]
-            elif message[0] == "Assigning":
-                worker_status[message[2]] = message[1][3]
-                worker_started[message[2]] = time.time()
-            elif message[0] == "Completed":
-                if message[1] in worker_started:
-                    durations.append(time.time() - worker_started[message[1]])
-                    avg = sum(durations[:100]) / len(durations[:100])
-                    secs_left = (avg * tasks) / len(worker_status)
-                    td = datetime.timedelta(seconds=int(secs_left))
-                    done = datetime.datetime.now() + td
-                    tasks -= 1
-                    del worker_started[message[1]]
-                if message[1] in worker_status:
-                    del worker_status[message[1]]
-            elif message[0] == "Tasks":
-                tasks = message[1]
-                total = message[2]
+            status.parse_message(messages.get())
         if clear_counter > 10:
             stdscr.clear()
             clear_counter = 0
         clear_counter += 1
         stdscr.erase()
-        stdscr.addstr(0, 0, f"Fitting images on {data_nas} in {data_folders}")
-        stdscr.addstr(1, 0, f"Saving fits to {save_nas} in {save_folder}")
-        stdscr.addstr(2, 0, f"Deconvolving with {psf_file}")
-        stdscr.addstr(3, 0, f"Flat field correcting with {flat_field_fl}")
+        stdscr.addstr(0, 0, f"Fitting images on {config['data_nas']} in {config['data_folders']}")
+        stdscr.addstr(1, 0, f"Saving fits to {config['save_nas']} in {config['save_folder']}")
+        stdscr.addstr(2, 0, f"Deconvolving with {config['psf_file']}")
+        stdscr.addstr(3, 0, f"Flat field correcting with {config['flat_field_fl']}")
         stdscr.addstr(4, 0, "-----------")
-        if scanning:
-            stdscr.addstr(5, 0, f"{tasks} files left to generate, scanning {scanning}")
-        else:
-            pct = 100 * (total - tasks) / total
-            stdscr.addstr(5, 0, f"{tasks} files left to generate, {pct:0.1f}% complete")
-        if done:
-            stdscr.addstr(6, 0, f"Estimated completion in {td} at {done}")
-        else:
-            stdscr.addstr(6, 0, "Estimated completion: TBD")
+        stdscr.addstr(5, 0, status.progress())
+        stdscr.addstr(6, 0, status.completion())
         stdscr.addstr(7, 0, "-----------")
-        if not worker_status:
-            stdscr.addstr(8, 0, "Waiting for task requests")
-        for i, (name, status) in enumerate(worker_status.items(), start=8):
-            duration = datetime.timedelta(seconds=int(time.time() - worker_started[name]))
-            stdscr.addstr(i, 0, f"{name}: {status} ({duration})")
-        stdscr.addstr(10 + len(worker_status), 0, "Press Q to quit")
+        for i, line in enumerate(status.workers(), start=8):
+            stdscr.addstr(i, 0, line)
+        stdscr.addstr(i + 2, 0, "Press Q to quit")
         stdscr.refresh()
         key = stdscr.getch()
         if key >= 0:
@@ -189,7 +207,7 @@ def interface(stdscr, messages):
 messages = multiprocessing.Queue()
 p = multiprocessing.Process(target=server_process, kwargs={"messages": messages})
 p.start()
-curses.wrapper(interface, messages)
+curses.wrapper(main, messages)
 p.terminate()
 p.join()
 p.close()
